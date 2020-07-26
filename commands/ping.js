10228d255cfaa31ab1d83dba4c22005887271fba
Athyen <athy3n@gmail.com>
2020-07-27 00:00:07 +0200

    Remove unnecessary comment

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+exports.run = async (client, message, args, level) => {
   const msg = await message.channel.send('Ping?')
   msg.edit(`Pong! Opóźnienie to ${msg.createdTimestamp - message.createdTimestamp}ms. Opóźnienie API to ${Math.round(client.ws.ping)}ms`)
 }
