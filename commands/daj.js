15de0cb9d7238bf26248443176411a718f35310b
Athyen <athy3n@gmail.com>
2020-07-26 21:00:12 +0200

    Add check for existence of argument and gender support

--- commands/daj.js
+++ commands/daj.js
@@ -1,13 +1,15 @@
 exports.run = (client, message, args, level) => {
-  if (client.normalize(args[0]) === 'sub' || client.normalize(args[0]) === 'suba') {
-    const sub = message.guild.roles.cache.get('562331914959323176')
-    if (message.member.roles.cache.has(sub.id)) {
-      message.channel.send('To miłe, że chcesz po raz drugi dać suba szamanom. Podziwiam Twój entuzjazm!')
-    } else {
-      message.member.roles.add(sub)
-      message.channel.send('Właśnie dałeś/aś suba szamanom!')
-    }
-  } else message.channel.send('Coś poszlo nie tak. Aby dać suba użyj komendy /daj suba')
+  if (!args[0]) {
+    if (client.normalize(args[0]) === 'sub' || client.normalize(args[0]) === 'suba') {
+      const sub = message.guild.roles.cache.get('562331914959323176')
+      if (message.member.roles.cache.has(sub.id)) {
+        message.channel.send('To miłe, że chcesz po raz drugi dać suba szamanom. Podziwiam Twój entuzjazm!')
+      } else {
+        message.member.roles.add(sub)
+        message.channel.send(`Właśnie ${message.member.roles.cache.has('555782372495196189') ? 'dałeś' : message.member.roles.cache.has('555782266689945611') ? 'dałaś' : 'dałeś/aś'} suba szamanom!`)
+      }
+    } else message.channel.send('Coś poszło nie tak. Aby dać suba użyj komendy /daj suba')
+  } else message.channel.send('Coś poszło nie tak. Aby dać suba użyj komendy /daj suba')
 }
 
 exports.conf = {
