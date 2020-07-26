dbe972fc6db63b41048a2d58e5b058e488e9f023
Athyen <athy3n@gmail.com>
2020-07-26 23:58:34 +0200

    Add check for existence of argument and change variable names

--- commands/jestem.js
+++ commands/jestem.js
@@ -1,25 +1,30 @@
 exports.run = (client, message, args, level) => {
-  const boi = message.guild.roles.cache.get('555782372495196189')
-  const gurl = message.guild.roles.cache.get('555782266689945611')
+  const warnMsg = 'coś poszło nie tak. Użyj komendy /jestem chłopcem lub /jestem dziewczynką'
+
+  if (!args[0]) return message.reply(warnMsg)
+
+  const boiRole = message.guild.roles.cache.get('555782372495196189')
+  const gurlRole = message.guild.roles.cache.get('555782266689945611')
+
   if (client.normalize(args[0]) === 'chlopcem') {
-    if (message.member.roles.cache.has(boi.id)) {
+    if (message.member.roles.cache.has(boiRole.id)) {
       message.channel.send('Co Ty kombinujesz? Przecież jesteś już chłopcem...')
-    } else if (message.member.roles.cache.has(gurl.id)) {
+    } else if (message.member.roles.cache.has(boiRole.id)) {
       message.channel.send('Wypad mi z tym gender fluid!')
     } else {
-      message.member.roles.add(boi)
+      message.member.roles.add(boiRole)
       message.channel.send('Gratulacje, właśnie zostałeś chłopcem!')
     }
   } else if (client.normalize(args[0]) === 'dziewczynka') {
-    if (message.member.roles.cache.has(gurl.id)) {
+    if (message.member.roles.cache.has(gurlRole.id)) {
       message.channel.send('Co Ty kombinujesz? Przecież jesteś już dziewczynką...')
-    } else if (message.member.roles.cache.has(boi.id)) {
+    } else if (message.member.roles.cache.has(gurlRole.id)) {
       message.channel.send('Wypad mi z tym gender fluid!')
     } else {
-      message.member.roles.add(gurl)
+      message.member.roles.add(gurlRole)
       message.channel.send('Gratulacje, właśnie zostałaś dziewczynką!')
     }
-  } else message.channel.send('Coś poszło nie tak. Użyj komendy /jestem chłopcem lub /jestem dziewczynką')
+  } else message.reply(warnMsg)
 }
 
 exports.conf = {
